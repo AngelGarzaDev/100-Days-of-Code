987692ce6353382473317925f787fac660be57f1
Angel Gonzalez Garza <aagarza@proton.me>
2023-12-18 02:19:00 -0500

    Created band name generator

---
+++ day001-band-name-generator.py
@@ -0,0 +1,7 @@
+# In this lesson I learned how to use the print function, input function, and variables. 
+# I also learned that creating a new line in strings is defined by \n, and the + symbol can be used to add strigns together.
+
+print("Hello! I will help you create a band name. Let's get started.")
+city = input("Which city did you grow up in?\n")
+petname = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + petname + ".")
