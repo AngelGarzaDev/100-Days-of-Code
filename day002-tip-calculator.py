3868599008a3f084052679f5f07bf5461b7f3335
Angel Gonzalez Garza <aagarza@proton.me>
2023-12-18 20:45:45 -0500

    Added rounding to improve human readability

--- day002-tip-calculator.py
+++ day002-tip-calculator.py
@@ -2,8 +2,8 @@
 # Lastly I learned that f-print allows us to print different data types as strings, helping to shorten code.
 
 print("Welcome to the tip calculator.")
-total_bill = int(input("What was the total bill?"))
-percentage_tip = int(input("What percentage would you like to tip?"))
+total_bill = float(input("What was the total bill?"))
+percentage_tip = float(input("What percentage would you like to tip?"))
 number_of_people = int(input("How many people are splitting the bill?"))
-each_pay = ((percentage_tip / 100 * total_bill ) + total_bill) / number_of_people
+each_pay = round(((percentage_tip / 100 * total_bill ) + total_bill) / number_of_people, 2)
 print(f"Each person should pay: ${each_pay}")
